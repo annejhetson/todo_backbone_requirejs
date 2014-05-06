19a0ef1471cb665ca43ff052ea4d16bcedee5f74
Anne Jhetson <student@epicodus.com>
2014-05-06 16:21:22 -0700

    user can add todo from home page

--- views/collectionview.js
+++ views/collectionview.js
@@ -1,19 +1,48 @@
 var TodoListView = Backbone.View.extend({
-  
+  el: '#container',
+  events: {
+    "click button": "createTodoOnEnter"
+  },
+
   initialize: function(){
     this.collection.on('add', this.addOne, this);
     this.collection.on('reset', this.addAll, this);
-    
+    this.collection.fetch();
     //not 100% sure about fetch and reset here
   },
-  //when run 
-  
+
+
+  createTodoOnEnter: function(e){
+    var newTodo = $('#new-todo').val();
+    // console.log(todoList.at(0).get('description'));
+    todoList.create({description: newTodo});
+    // var todo = new TodoItem();
+    // todo.set('description', newTodo);
+    // todoList.add(todo);
+
+
+    // if(e.keyCode != 13) return;
+    // if (!this.$el.find("#new-todo").val()) return;
+    //     this.todoList.create({title: this.$el.find("#new-todo").val(),completed: false}); // sync new model and add to our Todos collection
+    //     this.$el.find("#new-todo").val(''); //clear input box
+    //     return this;
+    // },
+
+  // newAttr: function(){
+  //   return {
+  //     title: this.input.val().trim(),
+  //     completed: false
+  //   }
+  },
+  //when run
+
   addOne: function(todoItem) {
     var todoView = new TodoView({model: todoItem});
     this.$el.append(todoView.render().el);
   },
 
   addAll: function(){
+    this.$('#container').html('');
     this.collection.forEach(this.addOne, this);
   },
 
